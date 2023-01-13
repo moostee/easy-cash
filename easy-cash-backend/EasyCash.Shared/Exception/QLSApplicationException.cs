d7cbc85d48c837810b6046089503b7fa1975cd59
Mustapha Akinsola Omotunwase <mustaphaakinsola.com>
2023-01-13 12:57:43 +0100

    set up shared framework/dependencies

---
+++ easy-cash-backend/EasyCash.Shared/Exception/QLSApplicationException.cs
@@ -0,0 +1,59 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using FluentValidation.Results;
+
+namespace EasyCash.Shared.Exceptions
+{
+    public class EasyCashException : Exception
+    {
+        public string StatusCode { get; }
+        public string StatusMessage { get; set; }
+        [Display(Name = "Message")]
+        public string MainMessage { get; set; }
+        public IDictionary<string, string[]> ValidationErrors { get; }
+
+
+        public string FormattedError { get; }
+
+
+        protected EasyCashException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+        public EasyCashException(string message) : base(message)
+        {
+            StatusCode = StatusCodes.INVALID_REQUEST;
+            StatusMessage = message;
+            MainMessage = message;
+        }
+
+
+        public EasyCashException(List<ValidationFailure> failures)
+            : this("One or more validation failures have occurred.")
+        {
+            ValidationErrors = new Dictionary<string, string[]>();
+            IEnumerable<string> propertyNames = failures
+                .Select(e => e.PropertyName)
+                .Distinct();
+
+            foreach (string propertyName in propertyNames)
+            {
+                string[] propertyFailures = failures
+                    .Where(e => e.PropertyName == propertyName)
+                    .Select(e => e.ErrorMessage)
+                    .ToArray();
+
+                FormattedError += String.Join(",", failures
+                    .Where(e => e.PropertyName == propertyName)
+                    .Select(e => e.ErrorMessage)) + ", ";
+
+                ValidationErrors.Add(propertyName, propertyFailures);
+            }
+        }
+
+
+    }
+
+}
